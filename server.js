83622b6f8526dbae8d705491d8e654b61d218a26
xunvyre <victoria@thedavids.us>
2022-10-11 14:10:44 -0400

    server connection tested successfully

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.static('public'));
-//app.use(require('./routes'));
+//app.use(require('./routes')); 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt-db',
 {
