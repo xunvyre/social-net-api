59be7dad9207c22911dd4b47da4a6f799fe25958
xunvyre <victoria@thedavids.us>
2022-10-11 14:10:01 -0400

    files structured, basic code copied from pizza-hunt

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(require('./routes'));
+//app.use(express.static('public'));
+//app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt-db',
 {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 //log mongo queries
