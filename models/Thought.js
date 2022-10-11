a793f097a3e43ef82f7b2dfeab91baf50e0a4920
xunvyre <victoria@thedavids.us>
2022-10-11 14:35:29 -0400

    moment.js selected for date formatting, models created following project requirements

---
+++ models/Thought.js
@@ -0,0 +1,78 @@
+const {Schema, model, Types} = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema
+(
+    {
+        reactionId:
+        {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody:
+        {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username:
+        {
+            type: String,
+            required: true
+        },
+        createdAt:
+        {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM Do, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON:
+        {
+            getters: true
+        },
+        id: false
+    }
+);
+
+const ThoughtSchema = new Schema
+(
+    {
+        thoughtText:
+        {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt:
+        {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM Do, YYYY [at] hh:mm a')
+        },
+        username:
+        {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+ThoughtSchema.virtual('reactionCount').get(function()
+{
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
